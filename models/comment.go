e7f100824665ce5d5fce51e3c31979d7671b8e0d
Diva2504 <64818972+Diva2504@users.noreply.github.com>
2022-05-10 23:51:13 +0800

    add comment model, social media model, repo comment

--- models/comment.go
+++ models/comment.go
@@ -1,1 +1,12 @@
 package models
+
+import (
+	"gorm.io/gorm"
+)
+
+type Comment struct {
+	gorm.Model
+	UserID  uint   `gorm:"not null" json:"user_id"`
+	PhotoID uint   `gorm:"not null" json:"photo_id"`
+	Content string `gorm:"type:text" json:"message" binding:"required"`
+}
